1a37d81d544624d4369600bbb0762a1da893dfc7
yosassenav <mromerovanessa.soy@gmail.com>
2025-11-13 22:44:57 -0500

    implementing logic for POST 'endpoint' to post todos

---
+++ app/api/todos/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from '@/app/lib/prisma';
+import { NextResponse } from 'next/server';
+
+// POST new todo
+export async function POST(req: Request) {
+
+    try {
+        const body = await req.json();
+        const { title, description } = body;
+
+        if (!title) {
+            return NextResponse.json({ error: 'Title is required' }, { status: 400 });
+        }
+
+        const newTodo = await prisma.todo.create({
+            data: {
+                title,
+                description,
+                completed: false,
+            },
+        });
+
+        return NextResponse.json(newTodo, { status: 201 });
+
+    } catch (error: any) {
+        console.error('Error creating todo', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+}
