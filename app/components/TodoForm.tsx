b8d914dcfec3a581e28e637a4f1ecf258ff6f381
yosassenav <mromerovanessa.soy@gmail.com>
2025-11-13 19:21:17 -0500

    TodoForm implementation

--- app/components/TodoForm.tsx
+++ app/components/TodoForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface Todo {
     title: string,
@@ -10,9 +10,47 @@ interface Todo {
 
 export const TodoForm = () => {
     const [todo, setTodo] = useState<Todo>({ title: '', description: '' });
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+
+        // with prev we have the latest state
+        setTodo((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        if (!todo.title.trim()) {
+            alert('Todo title cannot be empty');
+            return;
+        }
+    }
+
     return (
-        <form>
-            <input />
+        <form onSubmit={handleSubmit} className="">
+            <input
+                type="text"
+                name="title"
+                placeholder="todo title"
+                value={todo.title}
+                onChange={handleChange}
+                className=""
+                required
+            />
+            <textarea
+                name="description"
+                placeholder=""
+                value={todo.description}
+                onChange={handleChange}
+                className=""
+            />
+            <button
+                type="submit"
+                className=""
+            >
+                Add Todo
+            </button>
         </form>
     )
 }
