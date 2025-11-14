3f1533ea0aa75a29097f19b6bb65adc5ab931ce7
yosassenav <mromerovanessa.soy@gmail.com>
2025-11-14 00:23:21 -0500

    adding DELETE 'endpoint' to delete todos

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -34,7 +34,9 @@ export async function GET() {
         const todos = await prisma.todo.findMany({
             orderBy: { createdAt: 'desc' },
         })
+
         return NextResponse.json(todos, { status: 200 });
+
     } catch (error: any) {
         console.error('Error fetching todos', error);
         return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
@@ -42,3 +44,25 @@ export async function GET() {
 }
 
 
+//Delete todo
+export async function DELETE(req: Request) {
+
+    //deleting todo by using query params
+    try {
+        const { searchParams } = new URL(req.url);
+        const id = searchParams.get('id');
+
+        if (!id) {
+            return NextResponse.json({ error: 'TODO id is required' }, { status: 400 })
+        }
+        const deletedTodo = await prisma.todo.delete({
+            where: { id: Number(id) },
+        })
+        return NextResponse.json(deletedTodo, { status: 200 });
+
+    } catch (error: any) {
+        console.error('Error deleting todo', error);
+        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
+    }
+
+}
