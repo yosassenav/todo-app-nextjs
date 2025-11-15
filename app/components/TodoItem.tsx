aadc69dae1dff4f87131f07034dc4e7d246721ab
yosassenav <mromerovanessa.soy@gmail.com>
2025-11-15 01:10:35 -0500

    adding implementation for TodoItem

--- app/components/TodoItem.tsx
+++ app/components/TodoItem.tsx
@@ -1,18 +1,46 @@
 'use client';
 
-export const TodoItem = ({ todo, onDelete, onToggleComplete }) => {
+import { headers } from "next/headers";
+
+interface Todo {
+    id: number;
+    title: string;
+    description?: string;
+    completed: boolean;
+
+}
+
+interface TodoItemProps {
+    todo: Todo,
+    onDelete: (id: number) => void,
+    onToggleComplete: (id: number, completed: boolean) => void,
+}
+
+export function TodoItem({ todo, onDelete, onToggleComplete }: TodoItemProps) {
     const handleDelete = async () => {
-        await fetch('', {});
+        await fetch(`/api/todos/${todo.id}`, {
+            method: 'DELETE',
+        });
+
+        onDelete(todo.id);
     }
 
     const handleToggleComplete = async () => {
+        await fetch(`/api/todos/${todo.id}`, {
+            method: 'PATCH',
+            body: JSON.stringify({ completed: !todo.completed }),
+            headers: { "Content-Type": "application/json" },
+        });
 
+        onToggleComplete(todo.id, !todo.completed);
     }
     return (
-        <div>
-            <p>title</p>
-            <p>description</p>
-
-        </div>
+        <li>
+            <span className="">{todo.title}</span>
+            <button onClick={handleToggleComplete}>
+                {todo.completed ? "undo" : "complete"}
+            </button>
+            <button onClick={handleDelete}>Delete</button>
+        </li>
     )
 }
